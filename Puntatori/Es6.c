9c23b382bc01840f22dc565dbabacdcdefccf9b8
Arielli Andrea <ar.andrea04@gmail.com>
2025-11-03 22:45:08 +0100

    Edits

--- Puntatori/Es6.c
+++ Puntatori/Es6.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void stampa(int *a, int length);
+
 int main() {
     int n, Max_Char = 30;
-    printf("quanti nomi vuoi inserire? ");
+    printf("quanti valori vuoi inserire? ");
     scanf("%d",&n);
     
-    char *arrNomi[n];
-    for (int i = 0; i < n; i++){
-        arrNomi[i] = malloc(Max_Char * sizeof(char));
-        scanf("%s",arrNomi[i]);
-    }
+    int *arrNomi = malloc(n * sizeof(int));
+
+    printf("Inseriscili: ");
+    for (int i = 0; i < n; i++) scanf("%d", arrNomi+i);
     
-    for (int i = 0; i < n; i++)
-        printf("hai inserito: %s, indirizzo: %p\n",arrNomi[i], (void*)arrNomi[i]);
+    int *arrNomi2 = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) *(arrNomi2 + i) = *(arrNomi + n - 1 - i);
     
-    for (int i = 0; i < n; i++)
-        free(arrNomi[i]);
+    stampa(arrNomi, n);
+    stampa(arrNomi2, n);
+    free(arrNomi2);
+    free(arrNomi);
     
     return 0;
+}
+
+void stampa(int *a, int length) {
+    for (int i = 0; i < length; i++) printf("%d ", *(a+i));
+    printf("\n");
+    return;
 }
